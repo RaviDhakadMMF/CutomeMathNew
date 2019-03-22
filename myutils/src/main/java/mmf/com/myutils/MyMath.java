641cc0779bc5b50c24fa6fb4738a555cc3b68eab
RaviDhakadMMF <mmf.ravidhakad@gmail.com>
2019-03-22 16:25:27 +0530

    Ravi Third

--- myutils/src/main/java/mmf/com/myutils/MyMath.java
+++ myutils/src/main/java/mmf/com/myutils/MyMath.java
@@ -1,4 +1,24 @@
 package mmf.com.myutils;
 
 public class MyMath {
+    public static int Plus(int a,int b){
+        return  a+b;
+    }
+
+    public static int Minus(int a,int b){
+        return  a-b;
+    }
+
+    public static int Times(int a,int b){
+        return  a*b;
+    }
+
+    public static float Div(int a,int b){
+        if (b == 0){
+            throw new IllegalArgumentException("Div by Zero eroor");
+        }else {
+            return  a/b;
+        }
+
+    }
 }
